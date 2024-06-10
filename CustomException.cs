e4eeb5219221dad5e08586aed195ee10ca119556
PradoWilder <p.o.maisonneuve@gmail.com>
2024-06-10 11:09:12 +0200

    ExceptionChallenge done 10-06-2024

---
+++ CustomException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExceptionChallenge
+{
+    // Définition de la CustomException qui hérite de Exception
+    public class CustomException : Exception
+    {
+        public DateTime ExceptionTime { get; }
+
+        public CustomException(string message) : base(message)
+        {
+            ExceptionTime = DateTime.Now;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExceptionChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Appel de la fonction ThrowError qui lève une CustomException
+                ThrowError();
+            }
+            catch (CustomException ex)
+            {
+                // Affichage des informations de l'exception dans la console
+                Console.WriteLine($"Exception levée à : {ex.ExceptionTime}");
+                Console.WriteLine($"Message de l'exception : {ex.Message}");
+            }
+        }
+
+        // Fonction qui lève une CustomException
+        static void ThrowError()
+        {
+            throw new CustomException("Survenance d'une erreur personnalisée.");
+        }
+    }
+}
